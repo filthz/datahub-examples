97ffe92fa6f50924e12ceeac5eae39d7b1a8e9ce
Alex Tabaksmann <hury@gmx.net>
2021-01-03 11:24:22 +0100

    Update submit_data.py

--- submit_data.py
+++ submit_data.py
@@ -8,6 +8,7 @@ header = {"X-AUTH-TOKEN": auth_token, "Content-Type": "application/json"}
 data = {
   "value": 42
 }
+data = json.dumps(data)
 
 url = 'https://datahub.010102.de/uv1/api/sensor/' + sensor_id + '/measurement'
 response = requests.post(url, json=data, headers=header)
